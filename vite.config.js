6028350f2494c49329d791b8cc22b65da0a5309d
Yummuwu <a.cappello38@gmail.com>
2026-03-23 10:10:30 +0100

    23/03/2026

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 export default defineConfig({
-  base: '/portfolioE5/', // ⚠️ EXACTEMENT le nom du repo
-
   plugins: [react(), tailwindcss()],
-
   resolve: {
     alias: {
-      '@': new URL('./src', import.meta.url).pathname,
+      '@': path.resolve(__dirname, './src'),
     }
-  }
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
